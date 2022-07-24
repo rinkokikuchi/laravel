7b2a3bdb6ef4a3e6f94a74e7a1049f5672dadf8f
rinkokikuchi <raito0903a@gmail.com>
2022-07-25 08:14:21 +0900

    stripe決済テスト

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use App\Models\User;
+use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
@@ -21,11 +22,12 @@ class CartController extends Controller
             $totalPrice += $product->price * $product->pivot->quantity;
         }
 
-        //dd($products,$totalPrice);
 
         return view('user.cart',
         compact('products', 'totalPrice'));
     }
+
+
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id',$request->product_id) //cartクラスの中で探す($request->product_idを'product_id'と認識する)
@@ -45,6 +47,7 @@ class CartController extends Controller
         return redirect()->route('user.cart');
     }
 
+
     public function delete($id)
     {
         Cart::where('product_id' , $id)
@@ -53,6 +56,7 @@ class CartController extends Controller
         return redirect()->route('user.cart.index');
     }
 
+
     public function checkout()
     {
         $user = User::findOrFail(Auth::id()); //ログインしているユーザー情報を取得
@@ -60,16 +64,36 @@ class CartController extends Controller
 
         $lineItems=[];
         foreach($products as $product){
-            $lineItem=[
-                'name' => $product->name,
-                'description' => $product->information,
-                'amount' => $product->price,
-                'currency' => 'jpy',
-                'quantity' => $product->pivot->quantity,
-            ];
-            array_push($lineItems,$lineItem);
+            $quantity = '';
+            $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+
+
+            if($product->pivot->quantity > $quantity)
+            //カート内の数量 > stockテーブルの数量
+            {
+              //  return redirect()->route('user.cart.index');
+              //return redirect()->route('user.cart.index');
+              return view('user.cart.index',compact('product'));
+            }else{
+                $lineItem=[
+                    'name' => $product->name,
+                    'description' => $product->information,
+                    'amount' => $product->price,
+                    'currency' => 'jpy',
+                    'quantity' => $product->pivot->quantity,
+                ];
+                array_push($lineItems,$lineItem);
+            }
         }
-        //dd($lineItems);
+        foreach($products as $product)
+        {
+            Stock::create([
+                //カートに入れた分商品を減らしておく
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1
+            ]);
+        }
 
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
         $session = \Stripe\Checkout\Session::create([
@@ -79,9 +103,11 @@ class CartController extends Controller
             'success_url' => route('user.items.index'),
             'cancel_url' => route('user.cart.index')
         ]);
-        $publickey = env('STRIPE_PUBLIC_KEY');
+        $publicKey = env('STRIPE_PUBLIC_KEY');
 
-        return view('user.checkout' , compact('session', 'publickey'));
+
+
+        return view('user.checkout' , compact('session', 'publicKey'));
 
     }
 }
